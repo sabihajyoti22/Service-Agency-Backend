53918ca8d5bea0de80aa584207d3de68b94c4b5c
SabihaJyoti <sabihajyoti@gmail.com>
2022-08-03 21:21:36 +0600

    make it better

--- Config/passport.js
+++ Config/passport.js
@@ -6,7 +6,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:8080/auth/google/callback",
+			callbackURL: "https://service-agency-2022.herokuapp.com/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
