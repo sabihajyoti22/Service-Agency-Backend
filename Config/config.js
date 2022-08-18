89f9b09af7385da05f6e166deb1d978e17c2f6af
Sabiha Jyoti <you@example.com>
2022-08-18 21:45:47 +0600

    make it better

--- Config/config.js
+++ Config/config.js
@@ -6,10 +6,6 @@ const dev = {
     },
     db:{
         url: process.env.DATABASE || "mongodb://localhost:27017/"
-    },
-    googleAuth:{
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }
 }
 
